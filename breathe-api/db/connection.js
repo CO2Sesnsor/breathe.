dd7a5e66beb59857c4b2b4d0f0891a565d1c38b4
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-10-20 19:18:17 +0100

    backend posting and getting working

---
+++ breathe-api/db/connection.js
@@ -0,0 +1,8 @@
+const { createClient } = require("@supabase/supabase-js");
+
+const supabase = createClient(
+  "https://ohsakzptwvzxhpiutoke.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oc2FrenB0d3Z6eGhwaXV0b2tlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYyNjk4NDIsImV4cCI6MTk4MTg0NTg0Mn0.JIhHS34afnvOGpae6qTFtuzd12Ylugho73IwoUcDQVU"
+);
+
+module.exports = supabase;
---
+++ breathe-api/routes/api.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var router = express.Router();
+
+const supabase = require("../db/connection");
+
+/* GET home page. */
+router.get("/data", async function (req, res, next) {
+  const { data, error } = await supabase.from("data").select();
+  console.log(data);
+
+  res.send(data);
+  return "hello";
+});
+
+router.post("/data", async function (req, res, next) {
+  const { data, error } = await supabase.from("data").insert(req.body).select();
+
+  if (error) {
+    res.send(error);
+  } else {
+    res.send(data);
+  }
+});
+
+module.exports = router;

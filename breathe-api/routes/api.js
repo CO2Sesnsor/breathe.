0ab1f3690cbf5cb7903a4acb1e2a4213470840ec
Hasin Mahmood <hasinmahmood01@gmail.com>
2022-10-21 16:07:56 +0100

    posting from main page working using json

--- breathe-api/routes/api.js
+++ breathe-api/routes/api.js
@@ -1,6 +1,7 @@
 var express = require("express");
+const app = require("../app");
 var router = express.Router();
-
+router.use(express.json());
 const supabase = require("../db/connection");
 
 /* GET home page. */
@@ -13,7 +14,7 @@ router.get("/data", async function (req, res, next) {
 
 router.post("/data", async function (req, res, next) {
   const { data, error } = await supabase.from("data").insert(req.body).select();
-
+  console.log(req.body);
   if (error) {
     res.send(error);
   } else {
